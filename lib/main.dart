758b8765e4ec53909ebc50213b425181df465b7b
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-06-06 20:42:53 +0800

    [26] Create bloc for create_category
    Create bloc for create_category includes event and state

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
 import 'package:expense_tracker_app/app.dart';
 import 'package:expense_tracker_app/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
