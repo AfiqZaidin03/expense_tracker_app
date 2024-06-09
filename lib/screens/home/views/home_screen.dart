404a7802f11193b4ccd0aa961913c03e920efb33
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-06-09 14:53:51 +0800

    [45] Add Bloc builder and Bloc provider

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:expense_tracker_app/screens/expense/blocs/create_category_bloc/c
 import 'package:expense_tracker_app/screens/expense/blocs/create_expense_bloc/create_expense_bloc.dart';
 import 'package:expense_tracker_app/screens/expense/blocs/get_categories_bloc/get_categories_bloc.dart';
 import 'package:expense_tracker_app/screens/expense/views/expense.dart';
+import 'package:expense_tracker_app/screens/home/blocs/get_expense_bloc/get_expense_bloc.dart';
 import 'package:expense_tracker_app/screens/home/views/main_screen.dart';
 import 'package:expense_tracker_app/screens/stats/stats.dart';
 import 'package:flutter/cupertino.dart';
@@ -25,89 +26,108 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.vertical(
-          top: Radius.circular(30),
-        ),
-        child: BottomNavigationBar(
-          onTap: (value) {
-            setState(() {
-              index = value;
-            });
-          },
-          // backgroundColor: Colors.white,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          elevation: 3,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(
-                CupertinoIcons.home,
-                color: index == 0 ? selectedItem : unselectedItem,
+    return BlocBuilder<GetExpenseBloc, GetExpenseState>(
+      builder: (context, state) {
+        if (state is GetExpenseSuccess) {
+          return Scaffold(
+            bottomNavigationBar: ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30),
               ),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                CupertinoIcons.graph_square_fill,
-                color: index == 1 ? selectedItem : unselectedItem,
-              ),
-              label: 'Stats',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => MultiBlocProvider(
-                providers: [
-                  BlocProvider(
-                    create: (context) => CreateCategoryBloc(
-                      FirebaseExpenseRepo(),
+              child: BottomNavigationBar(
+                onTap: (value) {
+                  setState(() {
+                    index = value;
+                  });
+                },
+                // backgroundColor: Colors.white,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                elevation: 3,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      CupertinoIcons.home,
+                      color: index == 0 ? selectedItem : unselectedItem,
                     ),
+                    label: 'Home',
                   ),
-                  BlocProvider(
-                    create: (context) =>
-                        GetCategoriesBloc(FirebaseExpenseRepo())
-                          ..add(GetCategories()),
-                  ),
-                  BlocProvider(
-                    create: (context) => CreateExpenseBloc(
-                      FirebaseExpenseRepo(),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      CupertinoIcons.graph_square_fill,
+                      color: index == 1 ? selectedItem : unselectedItem,
                     ),
+                    label: 'Stats',
                   ),
                 ],
-                child: const AddExpense(),
               ),
             ),
-          );
-        },
-        shape: const CircleBorder(),
-        child: Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: LinearGradient(
-              colors: [
-                Theme.of(context).colorScheme.tertiary,
-                Theme.of(context).colorScheme.secondary,
-                Theme.of(context).colorScheme.primary,
-              ],
-              transform: const GradientRotation(pi / 4),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                var newExpense = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MultiBlocProvider(
+                      providers: [
+                        BlocProvider(
+                          create: (context) => CreateCategoryBloc(
+                            FirebaseExpenseRepo(),
+                          ),
+                        ),
+                        BlocProvider(
+                          create: (context) =>
+                              GetCategoriesBloc(FirebaseExpenseRepo())
+                                ..add(GetCategories()),
+                        ),
+                        BlocProvider(
+                          create: (context) => CreateExpenseBloc(
+                            FirebaseExpenseRepo(),
+                          ),
+                        ),
+                      ],
+                      child: const AddExpense(),
+                    ),
+                  ),
+                );
+
+                if (newExpense != null) {
+                  setState(() {
+                    state.expense.insert(0, newExpense);
+                  });
+                }
+              },
+              shape: const CircleBorder(),
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.tertiary,
+                      Theme.of(context).colorScheme.secondary,
+                      Theme.of(context).colorScheme.primary,
+                    ],
+                    transform: const GradientRotation(pi / 4),
+                  ),
+                ),
+                child: const Icon(
+                  CupertinoIcons.add,
+                ),
+              ),
             ),
-          ),
-          child: const Icon(
-            CupertinoIcons.add,
-          ),
-        ),
-      ),
-      body: index == 0 ? const MainScreen() : const StatScreen(),
+            body: index == 0 ? MainScreen(state.expense) : const StatScreen(),
+          );
+        } else {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
